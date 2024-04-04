ccf3abaf05a8b38b31d4d1f53e04faaa331152cd
ShepherdXAutomation <152513922+ShepherdXAutomation@users.noreply.github.com>
2024-04-04 21:42:10 +0000

    EOD 4-4

--- fading.py
+++ fading.py
@@ -15,9 +15,19 @@ custom_css = """
             opacity: 0;
         }
     }
+    .fade-in {
+     animation: fadeIn 1s ease-in-out forwards;
+    }
+    @keyframes fadeIn {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
 </style>
 """
-
+placeholder = st.empty()
 # Display the fading text
 st.markdown(custom_css, unsafe_allow_html=True)
 
@@ -28,5 +38,9 @@ for i in range(3):
     if i == 1:
         st.markdown("<p class='fade-out'>would you like to...</p>", unsafe_allow_html=True)
     if i == 2:
-        st.markdown("<p class='fade-out'>pray with me?</p>", unsafe_allow_html=True)
+       st.markdown("<p class='fade-out'>pray with me?</p>", unsafe_allow_html=True)
     time.sleep(3)
+
+st.empty()
+st.markdown("<p class='fade-in'>The prayer app takes details you give it and generates a personal prayer for you. Everything you share here is completely private.</p>", unsafe_allow_html=True)
+st.text_input("Tell me your troubles, my child.")
